752d3a1d010ee6be798a09fb4dd1ed5edde62c16
EC2 Default User <102804114+Natumesiki@users.noreply.github.com>
2022-08-29 14:01:18 +0000

    [finish]4-10

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -4,10 +4,10 @@ class PostImagesController < ApplicationController
   end
 
   def create
-    @post_image = PostImage.new(post_image_params)
-    @poat_image.user_id = current_user.id
+    @post_image = PostImage.new (post_image_params)
+    @post_image.user_id = current_user.id
     @post_image.save
-    redirect_to post_image_path
+    redirect_to post_images_path
   end
 
   def index
