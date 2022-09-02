b9a4709ade47e4de3d46ce11bf940ebbd716ec5c
EC2 Default User <102804114+Natumesiki@users.noreply.github.com>
2022-09-02 13:41:58 +0000

    [ADD]4-16

--- db/migrate/20220829052703_create_post_images.rb
+++ db/migrate/20220829052703_create_post_images.rb
@@ -7,8 +7,8 @@ class CreatePostImages < ActiveRecord::Migration[6.1]
      t.text :caption
     # add_reference :post_imeages, :user_id, null: false, foreign_key: true
     t.integer :user_id
-    
-    
+
+
       t.timestamps
     end
   end
